5f58e52a8c519fcad3acf2f1a60bc36c1ba6078a
Nandavaram-Mahesh <mahesh.nandavaram96@gmail.com>
2026-03-29 14:26:18 +0530

    Product , Order , Customer,Address Models Added

---
+++ src/database/models/Address.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const AddressSchema = new Schema({
+    street: String,
+    postalCode: String,
+    city: String,
+    country: String
+});
+
+const AddressModel =  mongoose.model('address', AddressSchema);
+
+export{AddressModel};
---
+++ src/database/models/Product.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema({
+    name: String,
+    desc: String,
+    banner: String,
+    type: String,
+    unit: Number,
+    price: Number,
+    available: Boolean,
+    suplier: String
+});
+
+const ProductModel =  mongoose.model('product', ProductSchema);
+
+export {ProductModel};
