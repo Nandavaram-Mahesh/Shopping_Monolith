b78d71556c38ef090fddc76e68989bc326e75263
Nandavaram-Mahesh <mahesh.nandavaram96@gmail.com>
2026-03-29 14:47:51 +0530

    error-handler middleware added to app

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 
 import { databaseConnection } from './database/connection.js'
+import { ErrorHandler } from './utils/error-handler.js'
 
 dotenv.config()
 
@@ -10,6 +11,9 @@ const StartServer = async()=>{
     const app = express()
 
     await databaseConnection()
+    
+
+    app.use(ErrorHandler)
     
     app.listen(process.env.PORT,()=>console.log('server is running on http://localhost:3000'))
 }
